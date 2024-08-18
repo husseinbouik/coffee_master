ac1b5c893e7ea9cd37bfd5b024df22f6a4565b10
husseinbouik <116066389+husseinbouik@users.noreply.github.com>
2024-08-18 17:41:16 +0100

    chore: Add Offerspage widget for displaying offers

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
+import 'package:coffee_master/offerspage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
 class HelloWord extends StatelessWidget {
   const HelloWord({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const Text("Hello World");
   }
-
 }
+
 class Greet extends StatefulWidget {
   const Greet({super.key});
 
@@ -19,31 +22,32 @@ class Greet extends StatefulWidget {
 }
 
 class _GreetState extends State<Greet> {
-// state variable 
-    var name = "";
+  // state variable 
+  var name = "";
 
   @override
   Widget build(BuildContext context) {
     var greetStyle = const TextStyle(fontSize: 24);
-    return 
-    Column(children: [ 
-    Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        children: [
-          Text("Hello $name ",style: greetStyle),
-          Text("???",style: greetStyle,),
-        ],
-      ),
-    ),
-    Padding(
-      padding: const EdgeInsets.only(right: 19.0,left: 20.0),
-      child: TextField(
-        onChanged: (value) => setState(() {
-          name = value;
-        })),
-    ),
-    ],
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              Text("Hello $name ", style: greetStyle),
+              Text("???", style: greetStyle),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 19.0, left: 20.0),
+          child: TextField(
+            onChanged: (value) => setState(() {
+              name = value;
+            }),
+          ),
+        ),
+      ],
     );
   }
 }
@@ -55,71 +59,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Coffee Masters',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.brown,
+        // Apply the colorScheme based on the primarySwatch
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.brown),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
- 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        // Directly use the primary color for the AppBar
+        backgroundColor: Theme.of(context).primaryColor,
+        title: Image.asset("images/logo.png"),
       ),
-      body: Greet(),
-    
+      body:  Offerspage(),
     );
   }
 }
